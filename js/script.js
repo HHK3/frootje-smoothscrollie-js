8052530a7f4e3d98b2553c41e57ea4e52b71ae22
HHK3 <joeltilon@live.nl>
2018-12-07 10:17:07 +0100

    V2.0 (New version with Arrows and Swipe)

--- js/script.js
+++ js/script.js
@@ -11,9 +11,14 @@ links.forEach( (item) => {
   linksArray.push(item);
 });
 
+const arrowLeft  = document.querySelector('.nav__arrow--left');
+const arrowRight = document.querySelector('.nav__arrow--right');
+
 const activate = (num) => {
   // Delete active satusses from the links
   deleteActive();
+  // Show or hide arrows
+  showHideArrows();
   // Activates the Link num
   linksArray[num].classList.add('nav--active');
   // Brings content of num foward
@@ -25,6 +30,66 @@ const deleteActive = () => {
     item.classList.remove('nav--active');
   });
 }
+
+
+
+// Script for Arrows
+// Function to activate the next section
+const next = () => {
+  if (index < linksArray.length-1) {
+    index++;
+  } else {
+    index = 0;
+  }
+  activate(index);
+}
+
+// Function to activate the previous section
+const prev = () => {
+  if (index > 0) {
+    index--;
+  } else {
+    // Index should never be under the 0
+    index = linksArray.length -1;
+  }
+  activate(index);
+}
+
+const showHideArrows = () => {
+
+  // Left Arrow
+  if (index == 0) {
+    arrowLeft.style.display = 'none';
+  } else {
+    arrowLeft.style.display = 'block';
+  }
+
+  // Right Arrow
+  if (index == linksArray.length -1) {
+    arrowRight.style.display = 'none';
+  } else {
+    arrowRight.style.display = 'block';
+  }
+}
+
+
+arrowRight.addEventListener('click', next);
+arrowLeft.addEventListener('click', prev);
+
+// Keyboard Events
+document.addEventListener('keyup', (e) => {
+  if(e.keyCode == 39 || e.keyCode == 32) {
+    next();
+  }
+  if (e.keyCode == 37) {
+    prev();
+  }
+})
+
+// Swipe Events
+document.addEventListener('swiped-left', next);
+document.addEventListener('swiped-right', prev);
+
 
 // At the start first item
 activate(index);
